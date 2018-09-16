44b852089fc847c179638b45f3361e3c3a06696b
acordeonl <acordeonlwebmaster@gmail.com>
2018-09-15 19:08:41 -0500

    add app header

--- src/App.js
+++ src/App.js
@@ -8,6 +8,19 @@ const s = {
     filters:{
         display:'flex',
         flexDirection:'row'
+    },
+    header:{
+        paddingTop:'45px',
+        paddingBottom:'30px'
+    },
+    headerContent:{
+        marginLeft:'42px',
+        width: '408px',
+        height: '22px',
+        fontFamily: 'Open Sans',
+        fontSize: '16px',
+        fontWeight: '600',
+        color: '#687b82',
     }
 }
 
@@ -33,6 +46,11 @@ class App extends Component {
         return (<div>
             <AppBar/>
             <ShowAppSearch onSubmit={this.handleSubmitSearch.bind(this)}/>
+            <div style={s.header}>
+                <div style={s.headerContent}>
+                    Descubra nuevas películas y programas de televisión
+                </div>
+            </div>
             <div style={s.filters}>
                 <div style={{marginLeft:'42px'}}>
                     <ComboBox disabled={this.state.yearFilterDisabled} title='Year' width='120' height='30' 
